898b8f0c9afee3ca480910a5409511bb2ad35d29
pengry <dogpenguin9@gmail.com>
2018-12-10 18:00:07 +0800

    add public param dic.

--- UseFunc/UseFunc/Program.cs
+++ UseFunc/UseFunc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 using static System.Console;
 
@@ -18,6 +19,8 @@ namespace UseFunc
             var results = funcWithLock.ASyncExecTasks<string, string>(lst, (item) => {
                 string result = string.Empty;
 
+                var dataTimeParam = GetDataTimeParam();
+
                 return result;
             });
 
@@ -25,5 +28,18 @@ namespace UseFunc
             WriteLine("Hello World!");
             ReadLine();
         }
+
+        public static SortedDictionary<string, string> GetDataTimeParam()
+        {
+            // 准备时间戳。ETC：1540220907287
+            DateTime dtFrom = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            long currentMillis = (DateTime.Now.Ticks - dtFrom.Ticks) / 10000;
+
+            return new SortedDictionary<string, string>() {
+            { "timestamp", currentMillis.ToString() }
+        };
+        }
     }
+
+    
 }
