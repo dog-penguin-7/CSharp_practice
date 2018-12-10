b3a020121f80dc9335130bb1d453e4c5b38a16df
pengry <dogpenguin9@gmail.com>
2018-12-10 17:21:10 +0800

    Use func.
    first try.

---
+++ UseFunc/UseFunc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+using static System.Console;
+
+using UseLock;
+
+namespace UseFunc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lst = new List<string>();
+            lst.Add("zhangsan");
+            lst.Add("lisi");
+
+            UseLockWithFunc funcWithLock = new UseLockWithFunc();
+            var results = funcWithLock.ASyncExecTasks<string, string>(lst, (item) => {
+                string result = string.Empty;
+
+                return result;
+            });
+
+
+            WriteLine("Hello World!");
+            ReadLine();
+        }
+    }
+}
---
+++ UseFunc/UseFunc/UseLock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace UseLock
+{
+    public class UseLock
+    {
+        public IList<TResult> ASyncExecTasks<TInput, TResult>(IList<TInput> lst, Func<TInput, TResult> invoke)
+        {
+            var tasks = new List<Task<TResult>>();
+            var results = new List<TResult>();
+            var dicASyncExecTaskPostUrl = new Dictionary<int, string>();
+
+            foreach (var item in lst)
+            {
+                Func<object, TResult> fun = (obj) => {
+
+                    TResult r = default(TResult);
+                    var mid = System.Threading.Thread.CurrentThread.ManagedThreadId;
+
+                    try
+                    {
+                        r = invoke((TInput)obj);
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteLine(ex.ToString());
+                    }
+
+                    //从字典中去除。
+                    lock (dicASyncExecTaskPostUrl)
+                    {
+                        dicASyncExecTaskPostUrl.Remove(mid);
+                    }
+
+                    return r;
+                };
+
+                var task = new Task<TResult>(fun, item);
+                tasks.Add(task);
+                task.Start();
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            //获取结果集。
+            foreach (var _task in tasks)
+            {
+                if (null != _task.Result)
+                {
+                    results.Add(_task.Result);
+                }
+                else
+                {
+                    //累加异常次数。
+                    WriteLine("失败 。。。");
+                }
+            }
+
+            dicASyncExecTaskPostUrl.Clear();
+            dicASyncExecTaskPostUrl = null;
+
+            return results;
+        }
+    }
+}
